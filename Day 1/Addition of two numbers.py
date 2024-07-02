252b8ad7d3e7f4d655018c32eb50913add831dd2
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-02 15:06:09 -0700

    Create Addition of two numbers.py

---
+++ Day 1/Addition of two numbers.py
@@ -0,0 +1,8 @@
+'''
+    This is a simple python program that asks the user for two numbers and finds their sum.
+'''
+
+firstNumber = float(input("Enter the first number: "))
+secondNumber = float(input("Enter the second number: "))
+
+print(f"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
