ec3edb70a9438246abaf5d41cf9b1e2214eea1ac
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-03 09:11:21 -0700

    Area of a triangle
    This area of a triangle is calculated based on the mathematical concept of Heron's formula. Note that it is not applied for Right Angled triangles

---
+++ Day 3/Area of a triangle.py
@@ -0,0 +1,22 @@
+'''
+    This is a simple program to find the area of a triangle.
+    -> Here, we will ask the 3 sides from the user and calculate the area of the traingle using
+       a mathematical concept known as Heron's Formula
+'''
+
+import math
+a = float(input("Enter the length of the first side: "))
+b = float(input("Enter the length of the second side: "))
+c = float(input("Enter the length of the third side: "))
+
+s = a + b + c
+
+Area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+decimal_places = int(input("Enter the number of decimal places: "))
+print(f"Area of triangle with sides a = {a}, b = {b}, and c = {c} is: {Area:.{decimal_places}f} units^2")
+
+'''
+    NOTE : This area of a triangle is calculated based on the mathematical concept of Heron's formula. 
+    Note that it is not applied for Right Angled triangles
+    
+'''
