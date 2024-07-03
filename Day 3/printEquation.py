c76a737d0420ecb5fae129eeacfdadb60aca2967
Maximus <164879113+Maximus-Ay@users.noreply.github.com>
2024-07-03 03:04:06 -0700

    Update printEquation.py

--- Day 3/printEquation.py
+++ Day 3/printEquation.py
@@ -1,9 +1,9 @@
 def printEquation(a,b,c):
     if a == 1 and b == 1:
-        print(f"x^2 + x + {c} = 0")
+        print(f"x\u00B2 + x + {c} = 0")
     elif a == 1:
-        print(f"x^2 + {b}x + {c} = 0")
+        print(f"x\u00B2 + {b}x + {c} = 0")
     elif b == 1:
-        print(f"{a}x^2 + x + {c} = 0")
+        print(f"{a}x\u00B2 + x + {c} = 0")
     else:
-        print(f"{a}x^2 + {b}x + {c} = 0")
+        print(f"{a}x\u00B2 + {b}x + {c} = 0")
