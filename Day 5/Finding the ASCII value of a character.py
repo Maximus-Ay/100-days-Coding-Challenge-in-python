e4760b1eced0ce44be7545d6a34517359f640540
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-05 09:51:56 -0700

    ASCII value of a character
    Here I made use of the ord() function or method in python that did the rest for me.

---
+++ Day 5/Finding the ASCII value of a character.py
@@ -0,0 +1,11 @@
+'''
+    This is simple program that asks the user for a character and finds the ASCII value of that character.
+    To do this we will make use of the ord() function in python that was defined specifically for that.
+
+'''
+
+character = input("Enter a character: ")
+
+print(f"The ASCII value of {character} is {ord(character)}")
+
+
