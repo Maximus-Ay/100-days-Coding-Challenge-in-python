2a93916acdff979d59ea7d31096420e733892d4b
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-23 18:38:11 -0700

    Update printing a right angled triangle pattern.py

---
+++ Day23/printing a right angled triangle pattern.py
@@ -0,0 +1,11 @@
+'''
+    This is a simple python program that makes use of loops, to print a simple 
+    pattern below:
+        * 
+        * *
+        * * *
+        * * * *
+        * * * * * 
+'''
+
+number = int(input("Enter the length of the triangle(dimensions or height): "))
