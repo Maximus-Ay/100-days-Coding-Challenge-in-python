61c711077f2153b8c5ac989d3eb5fbb85f12d6b9
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-23 19:04:48 -0700

    Create printing the pattern of a normal triangle using loops.py

---
+++ Day23/printing the pattern of a normal triangle using loops.py
@@ -0,0 +1,11 @@
+'''
+    This is a simple python progran that makes use of the notion of loops to be able to print
+    the pattern of the following triangle:
+                                 *
+                                * *
+                               * * * 
+                              * * * * 
+                             * * * * * 
+'''
+
+length = int(input("Enter the length of the triangle you wish to print: "))
