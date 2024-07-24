f62e4536ee39c04a1e6019b104c087d908a67098
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-23 19:28:17 -0700

    Update printing the pattern of a normal triangle using loops.py

--- Day23/printing the pattern of a normal triangle using loops.py
+++ Day23/printing the pattern of a normal triangle using loops.py
@@ -8,4 +8,14 @@
                              * * * * * 
 '''
 
-length = int(input("Enter the length of the triangle you wish to print: "))
+length = int(input("Enter the length of the triangle(rows) you wish to print: "))
+for x in range(length):
+    # Column
+    # First for loop here is for the spaces
+    for y in range(length-x-1):
+        print(" ", end="")
+    # This second for loop is to print the stars
+    for j in range(x+1):
+        print("*", end=" ")
+    print()
+
