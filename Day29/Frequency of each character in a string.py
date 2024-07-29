8b1c0e16fef125bcf7eb6923e260e55b04f278eb
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-29 14:27:32 -0700

    Create Frequency of each character in a string.py

---
+++ Day29/Frequency of each character in a string.py
@@ -0,0 +1,8 @@
+'''
+    This is a simple program that finds the frequency of each character in a string
+    For this, the best suited structure to be used is the dictionary 
+
+'''
+
+string = input("Enter the string: ")
+
