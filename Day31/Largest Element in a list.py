8e11707ee4763b6e0bcc4ece6aece411862f35e6
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-31 21:47:48 -0700

    Largest element in a list

--- Day31/Largest Element in a list.py
+++ Day31/Largest Element in a list.py
@@ -3,4 +3,10 @@
 
 '''
 list = []
-print("Enter the elements of the list ")
+size = int(input("Enter the size of the list: "))
+print("Enter the elements of the list: ")
+for element in range(size):
+    userinput = int(input("Enter value: "))
+    list.append(userinput)
+list.sort()
+print(f"The largest element in the list is {list[size-1]}")
