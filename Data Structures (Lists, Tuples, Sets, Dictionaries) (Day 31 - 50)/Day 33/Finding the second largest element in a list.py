82286a84761c9e1740fde61d7d36f8ee50f29e1d
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-02 08:02:00 -0700

    Create Finding the second largest element in a list.py

---
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 33/Finding the second largest element in a list.py
@@ -0,0 +1,10 @@
+'''
+    This is a simple program that aims at finding the second largest element in a list.
+'''
+
+list = []
+size = int(input("Enter the number of elements the list should contain: "))
+for i in range(size):
+    userinput = int(input("Enter an element: "))
+    list.append(userinput)
+
