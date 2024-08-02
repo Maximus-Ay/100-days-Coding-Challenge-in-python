90f8e876d6027f6f9ec9da8c118f3f48f4b150b0
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-02 08:08:57 -0700

    Second smallest element in a list

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 33/Second smallest element in a list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 33/Second smallest element in a list.py
@@ -1,3 +1,12 @@
 '''
     This is a simple program to find the second smallest element in a list
 '''
+list = []
+size = int(input("Enter the number of elements the list should contain: "))
+for i in range(size):
+    userinput = int(input("Enter an element: "))
+    list.append(userinput)
+
+list.sort()
+print(f"The second largest element in the list is: {list[1]}")
+
