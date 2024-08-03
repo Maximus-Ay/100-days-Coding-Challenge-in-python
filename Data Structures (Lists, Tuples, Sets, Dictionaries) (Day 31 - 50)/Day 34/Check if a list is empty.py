59ecdfa3bd63ecdada6876fe283a4f71881266c4
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-03 09:16:19 -0700

    Update Check if a list is empty.py

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 34/Check if a list is empty.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 34/Check if a list is empty.py
@@ -1,3 +1,8 @@
 '''
     This is a simple program that finds if a list is empty
 '''
+elements = [1,2,3]
+if len(elements) == 0:
+    print("The list is empty")
+else:
+    print(f"The list is not empty, and contains {len(elements)} items")
