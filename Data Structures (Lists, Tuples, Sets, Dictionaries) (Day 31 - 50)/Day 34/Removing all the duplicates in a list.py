469d3940c63b9720fa40afdf5f088cfa387b4102
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-03 09:13:55 -0700

    Removing all duplicates from a list

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 34/Removing all the duplicates in a list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 34/Removing all the duplicates in a list.py
@@ -1,3 +1,12 @@
 '''
     This is a simple program that removes all the duplicates in a list
 '''
+
+elements = []
+size = int(input("Enter the size of the list: "))
+for element in range(size):
+    userinput = int(input(f"Enter element {element+1}: "))
+    elements.append(userinput)
+print(f"Before: {elements}")
+elements = set(elements)
+print(f"The list without duplicates is: {list(elements)}")
