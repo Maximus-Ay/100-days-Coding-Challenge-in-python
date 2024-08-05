60d0b4159de9388b68eab17eb659ebf327d28b8a
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-05 12:54:35 -0700

    Intersection between two lists

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 36/Intersection between two lists.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 36/Intersection between two lists.py
@@ -1,3 +1,22 @@
 '''
     This is a simple program that finds the intersection between two lists
 '''
+
+elements1 = []
+size1 = int(input("Enter the size of the list: "))
+for element in range(size1):
+    userinput = int(input(f"Enter element {element+1}: "))
+    elements1.append(userinput)
+
+elements2 = []
+size2 = int(input("Enter the size of the list: "))
+for element in range(size2):
+    userinput = int(input(f"Enter element {element+1}: "))
+    elements2.append(userinput)
+
+intersection = []
+for element in elements1:
+    if element in elements2:
+        intersection.append(element)
+
+print(f"The intersection between the two lists is: {intersection}")
