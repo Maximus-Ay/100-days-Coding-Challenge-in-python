0c26570439086ee18c5d88dbcf929b3ee3f94a74
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-05 12:51:57 -0700

    Difference between two lists in python

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 36/Difference between two lists.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 36/Difference between two lists.py
@@ -1,3 +1,24 @@
 '''
     This is a simple python program that finds the difference between two lists
 '''
+
+elements1 = []
+size1 = int(input("Enter the size of the list: "))
+for element in range(size1):
+    userinput = int(input(f"Enter element {element+1}: "))
+    elements1.append(userinput)
+
+elements2 = []
+size2 = int(input("Enter the size of the list: "))
+for element in range(size2):
+    userinput = int(input(f"Enter element {element+1}: "))
+    elements2.append(userinput)
+
+difference = []
+for element in elements1:
+    if element not in elements2:
+        difference.append(element)
+
+print(f"The difference between the two lists is: {difference}")
+
+
