960f685eddbabdec5c07015b44183b93f3a4c447
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-04 20:39:01 -0700

    Program to copy a list

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 35/Program to copy a list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 35/Program to copy a list.py
@@ -1,3 +1,12 @@
 '''
     This is a simple program that copies a list
 '''
+elements = []
+size = int(input("Enter the size of the list: "))
+for element in range(size):
+    userinput = int(input(f"Enter element {element+1}: "))
+    elements.append(userinput)
+print(f"Original list: {elements}")
+copy_of_list = elements
+print(f"Copy of list: {copy_of_list}")
+
