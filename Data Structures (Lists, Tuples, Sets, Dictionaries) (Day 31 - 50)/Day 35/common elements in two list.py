bb967e6098d2f6b45f69a85b6996046427b227ff
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-04 21:03:19 -0700

    Common elements between lists

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 35/common elements in two list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 35/common elements in two list.py
@@ -6,17 +6,18 @@
 elements1 = []
 size1 = int(input("Enter the size of the list: "))
 for element in range(size1):
-    userinput = int(input(f"Enter element {elements1+1}: "))
+    userinput = int(input(f"Enter element {element+1}: "))
     elements1.append(userinput)
 
 elements2 = []
 size2 = int(input("Enter the size of the list: "))
 for element in range(size2):
-    userinput = int(input(f"Enter element {elements2+1}: "))
+    userinput = int(input(f"Enter element {element+1}: "))
     elements2.append(userinput)
 common_elements = []
 for i in range(size1):
     for j in range(size2):
         if elements1[i] == elements2[j]:
-            common_elements.append(j)
-            
+            common_elements.append(elements2[j])
+
+print(f"common elements from both list are: {common_elements}")
