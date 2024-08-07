ac60dd722852b9f7fbc450f3e84b3675a6dd1e4a
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-07 09:41:25 -0700

    Count the number of elements in a list

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 38/Count the number of elements in a list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 38/Count the number of elements in a list.py
@@ -1,3 +1,15 @@
 '''
     This is a simple python program that finds the number of elements in a python lists
 '''
+def count(list):
+    count = 0
+    for x in list:
+        count+=1
+    return count
+    # Or just 
+    # return len(list)
+
+list = [1,2,3,4,5,6,7,2,5,3]
+print(f"List: {list}")
+
+print(f"The number of elements in the list is: {count(list)}")
