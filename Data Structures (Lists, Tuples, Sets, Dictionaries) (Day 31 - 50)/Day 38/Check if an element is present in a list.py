00438b9b5f7e5959b3872e8e7687cc9d4f2b47f0
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-07 09:50:45 -0700

    Check if an element is found in a list or not

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 38/Check if an element is present in a list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 38/Check if an element is present in a list.py
@@ -2,3 +2,16 @@
     This is a simple python program that checks to see if an element is present in a list
 '''
 
+list = [1,2,3,20,35,45,-89, 132]
+
+element = int(input("Enter the search element: "))
+
+if element in list:
+    print(element, "is found in list")
+    print("list:", list)
+else:
+    print(element, "is not found in list")
+    print("List:", list)
+
+
+
