c0c3cbf87acbb20264a58d56a98bb94653bd97aa
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-08 10:14:15 -0700

    Sorting la list in ascending order

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 39/Sorting a list in ascending order.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 39/Sorting a list in ascending order.py
@@ -3,3 +3,15 @@
     You can use built in function or just do it manually yourself
 '''
 
+def sorting_list_in_ascending_order(list):
+    return sorted(list)
+
+list = []
+size = int(input("Enter the size of the list: "))
+for x in range(size):
+    userinput = int(input(f"Enter element {x+1}: "))
+    list.append(userinput)
+    
+print(f"Before: {list}")
+print(f"The sorted list is: {sorting_list_in_ascending_order(list)}")
+
