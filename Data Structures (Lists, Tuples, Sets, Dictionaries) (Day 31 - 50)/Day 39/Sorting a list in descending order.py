7ceef4e31840662ab23918e93a8bdecde1c94383
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-08 10:19:58 -0700

    Sorting a list in descending order

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 39/Sorting a list in descending order.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 39/Sorting a list in descending order.py
@@ -2,3 +2,14 @@
     This is a python program that sorts a list in descending order. 
     We are going to use a built in function to be able to accomplish this
 '''
+def sorting_list_in_descending_order(list):
+    list = sorted(list)
+    return list[::-1]
+list = []
+size = int(input("Enter the size of the list: "))
+for i in range(size):
+    userinput = int(input(f"Enter element {i+1}: "))
+    list.append(userinput)
+
+print(f"Before: {list}")
+print(f"The list in descending order is: {sorting_list_in_descending_order(list)}")
