d5707493a875c5dd471cb3f7563a541c3fe6f4f5
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-09 14:18:15 -0700

    Maximum and minimum element in a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 40/Maximum and minimum element in a tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 40/Maximum and minimum element in a tuple.py
@@ -1,3 +1,8 @@
 '''
     This is a program to find the maximum and minimum element in a tuple
 '''
+
+tuple = (1,2,3,-4,0,89,-23)
+
+print(f"The maximum element in this tuple is: {max(tuple)}")
+print(f"The minimum element in this tuple is: {min(tuple)}")
