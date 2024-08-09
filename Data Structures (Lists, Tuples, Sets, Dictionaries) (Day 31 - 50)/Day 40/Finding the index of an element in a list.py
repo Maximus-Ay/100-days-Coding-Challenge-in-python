90aeed28033053a8fdcaa8febafe8801efcc4338
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-09 14:06:23 -0700

    Finding the index of an element in a list

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 40/Finding the index of an element in a list.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 40/Finding the index of an element in a list.py
@@ -2,3 +2,13 @@
     Finding the index of an element in a list
 '''
 
+list = []
+size = int(input("Enter the size of the list: "))
+for x in range(size):
+    userinput = int(input(f"Enter the element {x+1}: "))
+    list.append(userinput)
+
+search_element = int(input("Enter the search element: "))
+print(f"Index of element is: {list.index(search_element)}")
+
+
