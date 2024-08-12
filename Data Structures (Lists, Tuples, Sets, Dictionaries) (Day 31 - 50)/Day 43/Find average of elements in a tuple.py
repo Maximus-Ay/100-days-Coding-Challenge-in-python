6a8036f36d27f2e279d48a087c5fa9c4a3257705
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-12 09:59:33 -0700

    Average of elements in a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 43/Find average of elements in a tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 43/Find average of elements in a tuple.py
@@ -1,3 +1,7 @@
 '''
     This is a simple python program that finds the average of elements in a tuple
 '''
+
+tuple = (1,2,3,4,5,6,7,8,9,10)
+print(tuple)
+print(f"The average of the elements in the tuple is: {sum(tuple)/len(tuple)}")
