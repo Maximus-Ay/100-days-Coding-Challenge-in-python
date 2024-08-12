cc5789fd6e1f7ceb7e3ab49989e6727e4a92252c
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-11 20:04:41 -0700

    Turning a list into a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 42/Turn a list to tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 42/Turn a list to tuple.py
@@ -1,3 +1,10 @@
 '''
     This is a simple python program that transforms a list into a tuple
 '''
+
+list = [1,2,3,"hello", True]
+print(f"Before: {list}")
+# We are going to use the tuple method to type cast
+tuple = tuple(list)
+print(f"After: {tuple}")
+
