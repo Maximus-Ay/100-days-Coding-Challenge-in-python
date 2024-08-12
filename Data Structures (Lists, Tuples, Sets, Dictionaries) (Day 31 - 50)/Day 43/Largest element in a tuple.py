6b68c928004e530323e325e3dea980d3ec219ce1
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-12 10:04:05 -0700

    Largest element in a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 43/Largest element in a tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 43/Largest element in a tuple.py
@@ -1,3 +1,6 @@
 '''
     This is a simple program that finds the largest elements in a tuple
 '''
+tuple = (1,2,3,3,-7,10,90,-100,345,890)
+print(tuple)
+print(f"The largest element in the tuple is: {max(tuple)}")
