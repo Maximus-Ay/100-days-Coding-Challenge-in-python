8a2cde9b849b490aa6118012f0f35933d341a6a9
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-13 11:18:30 -0700

    Smallest element in a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 44/Smallest element in a tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 44/Smallest element in a tuple.py
@@ -1,3 +1,7 @@
 '''
     This is a simple python program that finds the smallest element in a tuple
 '''
+
+tuple = (1,2,3,4,-8,9,10)
+print(tuple)
+print(f"The smallest element in the tuple is: {min(tuple)}")
