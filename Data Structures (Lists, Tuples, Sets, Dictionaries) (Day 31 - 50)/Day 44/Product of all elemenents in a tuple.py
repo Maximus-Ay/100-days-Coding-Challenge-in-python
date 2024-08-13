f6b76b3c61ece41a59f99384de12c4912efea14e
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-13 11:25:47 -0700

    Product of all elements in a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 44/Product of all elemenents in a tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 44/Product of all elemenents in a tuple.py
@@ -1,3 +1,9 @@
 '''
     This is a simple program that finds the product of all elements in a tuplle
 '''
+tuple = (1,2,4)
+product = 1
+for x in tuple:
+    product *= x
+print(tuple)
+print(f"The product of all elements in the tuple is: {product}")
