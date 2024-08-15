2e16a2c20baa162199843fb6e0f8bb3d7d3631c0
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-14 20:28:28 -0700

    Remove all duplicates from tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 45/Remove duplicates from tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 45/Remove duplicates from tuple.py
@@ -2,3 +2,7 @@
     This is a simple python program that removes every duplicate from a tuple.
 '''
 
+myTuple = (1,2,3,4,2,5,6,1,2,7,9,2,3,4,8,9)
+print(f"Before: {myTuple}")
+myTuple = tuple(set(myTuple))
+print(f"After: {myTuple}")
