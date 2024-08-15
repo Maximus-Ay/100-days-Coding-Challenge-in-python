30b7732c70a2daacab12dc93f28821e6dbb80d87
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-15 13:31:23 -0700

    Max and Min element in a set

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 46/Max and min element in a set.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 46/Max and min element in a set.py
@@ -1,3 +1,8 @@
 '''
     This is a simply program that allows us to determine the maximum and minimum element in a set
 '''
+
+mySet = {1,2,3,4,1,4,-89,90,0-45} # Remember sets don't accept duplicates so even if you put one, it will not print it or consider it
+print(mySet)
+print(f"The maximum element in the set is: {max(mySet)}")
+print(f"The minimum element in the set is: {min(mySet)}")
