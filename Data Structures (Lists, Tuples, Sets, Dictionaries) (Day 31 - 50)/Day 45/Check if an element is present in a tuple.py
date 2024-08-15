6ecf4ef04ac7b2f461a554b54e3c936212945df3
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-14 20:32:32 -0700

    Check if an element is present within a tuple

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 45/Check if an element is present in a tuple.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 45/Check if an element is present in a tuple.py
@@ -1,3 +1,12 @@
 '''
     This is a simple program that checks if an element is found in a tuple
 '''
+
+myTuple = (1,2,3,5,6,8,9,10)
+searchElement = int(input("Enter the search element: "))
+if searchElement in myTuple:
+    print(f"{searchElement} is in the tuple!")
+    print(myTuple)
+else:
+    print(f"{searchElement} is not found within the list!")
+    print(myTuple)
