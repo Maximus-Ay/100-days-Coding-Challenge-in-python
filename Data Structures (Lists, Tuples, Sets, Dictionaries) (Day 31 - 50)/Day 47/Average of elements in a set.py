7fe62762ebc35866941f549bd45d4213c57995bd
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-16 12:12:44 -0700

    Average of the elements in a set

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 47/Average of elements in a set.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 47/Average of elements in a set.py
@@ -1,3 +1,7 @@
 '''
     This is a simple program that finds the average of all elements in a set
 '''
+mySet = {1,2,3,4,1}
+print(mySet)
+print(f"The sum of all elements in the set is: {sum(mySet)/len(mySet)}")
+print("Note that it doesn't consider the elements two times, since its a set and sets do not accept duplicates")
