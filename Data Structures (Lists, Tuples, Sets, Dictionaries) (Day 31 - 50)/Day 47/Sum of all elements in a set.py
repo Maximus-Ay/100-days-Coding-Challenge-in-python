135f76dbed445de04265be8e5e7c63c60541b7f2
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-16 12:00:29 -0700

    Sum of all elements in a set

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 47/Sum of all elements in a set.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 47/Sum of all elements in a set.py
@@ -1,3 +1,9 @@
 '''
     This is a simple progtam that finds the sum of all elements in a set
 '''
+
+mySet = {1,2,3,4,1}
+print(mySet)
+print(f"The sum of all elements in the set is: {sum(mySet)}")
+# Note: The answer will be 10, because it doesn't accept duplicates. Hence, only the first
+# occurence of each element will be considered.
