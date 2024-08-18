b2321966aaa283e18ab9e23a4a2e46e80cbc87c1
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-17 21:17:22 -0700

    Update Intersection of two sets.py

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 48/Intersection of two sets.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 48/Intersection of two sets.py
@@ -4,5 +4,5 @@
 set1 = {1,2,3,4,5,6,7}
 set2 = {1,4,5,9,10,22}
 print(f"Set 1 = {set1} and Set 2 = {set2}")
-union = set1.union(set2)
+union = set1.intersection(set2)
 print(f"The union of the two sets are: {union}")
