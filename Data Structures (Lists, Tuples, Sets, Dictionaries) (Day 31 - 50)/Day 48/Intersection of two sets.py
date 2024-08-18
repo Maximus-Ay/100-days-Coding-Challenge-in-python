33046f5199b6abfcc7f1c393162e7654fdf217b0
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-17 21:11:03 -0700

    Intersection of two sets in python

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 48/Intersection of two sets.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 48/Intersection of two sets.py
@@ -1,3 +1,8 @@
 '''
     This is a simple program that finds the intesection of two sets
 '''
+set1 = {1,2,3,4,5,6,7}
+set2 = {1,4,5,9,10,22}
+print(f"Set 1 = {set1} and Set 2 = {set2}")
+union = set1.union(set2)
+print(f"The union of the two sets are: {union}")
