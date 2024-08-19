15647bb8e24e5a713108563e2960fe349a234503
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-18 20:44:48 -0700

    Difference between two sets in python

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 49/Difference of two sets.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 49/Difference of two sets.py
@@ -1,3 +1,8 @@
 '''
     This is a simple python program that finds the difference between two sets
 '''
+set1 = {1,2,3,4,5,6,7}
+set2 = {1,4,5,9,10,22}
+print(f"Set 1 = {set1} and Set 2 = {set2}")
+difference = (set1.difference(set2)).union(set2.difference(set1))
+print(f"The union of the two sets are: {difference}")
