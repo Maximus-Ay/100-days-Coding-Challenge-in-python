27546cb30291c70e8cf0748bf47ffa9dc786d898
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-18 20:48:50 -0700

    Symmetric difference between two sets in python

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 49/Symmetric difference between two sets.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 49/Symmetric difference between two sets.py
@@ -1,4 +1,8 @@
 '''
     This is a simple program that finds the symmetric difference between two sets
 '''
-
+set1 = {1,2,3,4,5,6,7}
+set2 = {1,4,5,9,10,22}
+print(f"Set 1 = {set1} and Set 2 = {set2}")
+difference = (set1.symmetric_difference(set2))
+print(f"The union of the two sets are: {difference}")
