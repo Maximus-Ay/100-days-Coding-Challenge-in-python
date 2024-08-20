4d4ced9eb6d8fc47b6c8bf6c49651e62ea4dffe3
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-20 10:47:34 -0700

    Function to find the sum of all numbers in a list

--- Functions (Day 51 to 60)/Day 51/Sum of all numbers in a list.py
+++ Functions (Day 51 to 60)/Day 51/Sum of all numbers in a list.py
@@ -1,3 +1,12 @@
 '''
     This is a simple python function that finds the sum of all elements in a list
 '''
+def MySum(mylist):
+    sum = 0
+    for x in mylist:
+        sum += x
+    return sum
+# OR USING SIMPLE BUILT IN FUNCTION WE CAN JUST 
+# return sum(mylist)
+
+print(MySum([1,2,4,5,2,5,-8,45,-90,100]))
