9e25bf2cadf338a5a2018db5b92f7c1d4978a4c6
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-19 21:59:16 -0700

    Check if a set is a superset of another

--- Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 50/Check if a set is a superset of another.py
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 50/Check if a set is a superset of another.py
@@ -1,3 +1,8 @@
 '''
     This is a simple python program that checks if a set is a superset of another
 '''
+
+set1 = {1,2,3,4,5,6,7}
+set2 = {1,4,7}
+print(f"Set1: {set1} and Set2: {set2}")
+print(f"is Set 1 a superset of Set 2? {set1.issuperset(set2)}")
