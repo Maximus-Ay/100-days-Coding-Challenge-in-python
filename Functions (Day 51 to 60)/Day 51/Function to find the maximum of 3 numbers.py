38f299c9f808377237da9959177db1fc7d15f61f
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-20 10:42:38 -0700

    Function to find the maximum of three numbers in python

--- Functions (Day 51 to 60)/Day 51/Function to find the maximum of 3 numbers.py
+++ Functions (Day 51 to 60)/Day 51/Function to find the maximum of 3 numbers.py
@@ -1,3 +1,16 @@
 '''
     This is a python function to find the maximum of three numbers
 '''
+
+def Max(num1, num2, num3):
+    return max(num1,num2,num3)
+# OR WE COULD CODE IT MANUALLY WITHOUT USING A BUILT-IN FUNCTIOn
+#   if num1 > num2 and num1 > num3:
+#       return num1
+#   elif num2 > num1 and num2 > num3:
+#       return num2
+#   elif num3 > num1 and num3 > num2:
+#       return num3
+#   else:
+#       return "All a numbers are equal"
+print(Max(2,2,2))
