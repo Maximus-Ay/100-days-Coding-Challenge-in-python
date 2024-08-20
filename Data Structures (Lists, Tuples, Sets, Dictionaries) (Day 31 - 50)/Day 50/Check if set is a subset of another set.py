09e33344f1b236d897e160429b676bb6c1b64f64
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-19 21:54:51 -0700

    Check if a set is a subset of another set

---
+++ Data Structures (Lists, Tuples, Sets, Dictionaries) (Day 31 - 50)/Day 50/Check if set is a subset of another set.py
@@ -0,0 +1,8 @@
+'''
+    This is a simple python program that checks to find if a set is a subset of another set
+'''
+
+set1 = {1,2,3,4,5,6,7}
+set2 = {1,4,7}
+print(f"Set1: {set1} and Set2: {set2}")
+print(set2.issubset(set1))
