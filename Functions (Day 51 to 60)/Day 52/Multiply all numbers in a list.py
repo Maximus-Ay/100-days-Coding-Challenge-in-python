018d7732e53b24bafabce52121b1b763ae4cb158
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-21 19:39:04 -0700

    Update Multiply all numbers in a list.py

--- Functions (Day 51 to 60)/Day 52/Multiply all numbers in a list.py
+++ Functions (Day 51 to 60)/Day 52/Multiply all numbers in a list.py
@@ -2,3 +2,11 @@
     This is a simple python program that multiplies all numbers in a list
 '''
 
+def Multiply(mylist):
+    product = 1
+    for x in mylist:
+        product *= x
+    return product
+list = [1,2,3,4]
+print(list)
+print(f"The product of all numbers in the list is {Multiply(list)}")
