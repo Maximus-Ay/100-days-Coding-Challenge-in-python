6c44a0bc667bd89d62c5abc391e32d1752e8815e
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-21 19:42:36 -0700

    Reverse of a string in python

--- Functions (Day 51 to 60)/Day 52/Reverse a string in python.py
+++ Functions (Day 51 to 60)/Day 52/Reverse a string in python.py
@@ -1,3 +1,9 @@
 '''
     This is a simple python program to reverse a string
 '''
+
+def Reverse_a_string(myString):
+    return myString[::-1]
+
+string = "Maximus"
+print(f"The reverse of {string} is {Reverse_a_string(string)}")
