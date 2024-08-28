40dd3966af1fc62937746618f165c3882b52e856
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-27 18:42:41 -0700

    Sum of all digits in a number

--- Functions (Day 51 to 60)/Day 57/Sum of the digits of a number.py
+++ Functions (Day 51 to 60)/Day 57/Sum of the digits of a number.py
@@ -2,3 +2,12 @@
     This is a simple python program that finds the sum of all the digits in a number
 '''
 
+def Sum_of_all_digits(number):
+    sum = 0
+    while number > 0:
+        x = number % 10
+        sum += x
+        number //= 10
+    return sum
+
+print(Sum_of_all_digits(345))
