24ba761520989108b9bf24de8d828afabe22c1c6
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-29 16:13:49 -0700

    Binary to Decimal conversion

--- Functions (Day 51 to 60)/Day 59/Binary to decimal.py
+++ Functions (Day 51 to 60)/Day 59/Binary to decimal.py
@@ -1,3 +1,15 @@
 '''
     This is a python program that convets from Binary to Decimal
 '''
+
+def Binary_to_Decimal(number):
+    exp = 0
+    Sum = 0
+    while number > 0:
+        x = number % 10
+        Sum = Sum + (x * pow(2, exp))
+        exp += 1
+        number //= 10
+    return Sum
+
+print(Binary_to_Decimal(1101))
