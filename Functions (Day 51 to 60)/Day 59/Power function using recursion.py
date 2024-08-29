d6db67262f167db9be630eaabb3b2551d214928a
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-29 14:43:16 -0700

    Power function using recursion

--- Functions (Day 51 to 60)/Day 59/Power function using recursion.py
+++ Functions (Day 51 to 60)/Day 59/Power function using recursion.py
@@ -2,3 +2,10 @@
     This is a simple python program that does the power function using recursion
 '''
 
+def power_function(base, exponent):
+    if exponent == 0:
+        return 1
+    else:
+        return base * power_function(base, exponent-1)
+
+print(power_function(4,3))
