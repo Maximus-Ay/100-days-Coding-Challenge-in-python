6566e3a27d2e3809f2824a6dfced122c41243660
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-28 20:12:56 -0700

    Sum of the squares of the first n natural numbers

--- Functions (Day 51 to 60)/Day 58/Sum of squares of first n natural numbers.py
+++ Functions (Day 51 to 60)/Day 58/Sum of squares of first n natural numbers.py
@@ -2,3 +2,12 @@
     This is a python program that finds the sum of the squares of the first n natural numbers
 '''
 
+def Sum_of_Squares(n):
+    sum = 0
+    for x in range(1,n+1):
+        sum = sum + pow(x, 2)
+    
+    return sum
+
+n = int(input("Enter the value of n: "))
+print(f"The sum of the first {n} natural numbers is: {Sum_of_Squares(n)}")
