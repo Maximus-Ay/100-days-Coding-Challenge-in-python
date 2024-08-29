55dab92cfb1b777a042a2a4b0d4e5f407a1a435f
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-29 16:24:54 -0700

    Decimal to Binary in python

--- Functions (Day 51 to 60)/Day 60/Decimal to Binary.py
+++ Functions (Day 51 to 60)/Day 60/Decimal to Binary.py
@@ -2,3 +2,15 @@
     This is a simple program that converts from Decimal to Binary
 '''
 
+def Decimal_to_Binary(number):
+    Binary_number = []
+    temp = number
+    while temp > 0:
+        x = temp % 2
+        Binary_number.append(x)
+        temp //= 2
+    
+    for x in reversed(Binary_number):
+        print(x,end='')
+
+Decimal_to_Binary(25)
