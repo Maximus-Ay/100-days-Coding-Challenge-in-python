16281af9fd6dd767b1ec12a99c99fcb9565c5d20
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-29 16:29:03 -0700

    Area of a circle given its radius

--- Functions (Day 51 to 60)/Day 60/Area of a circle given its radius.py
+++ Functions (Day 51 to 60)/Day 60/Area of a circle given its radius.py
@@ -1,4 +1,8 @@
 '''
     Simple python program to find the radius of a circle
 '''
+import math
+def Area_Of_A_Circle(radius):
+    return f"{math.pi * pow(radius, 2):.2f}cm^2"
 
+print(Area_Of_A_Circle(10))
