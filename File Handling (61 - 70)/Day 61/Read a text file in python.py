fc67591c5b63ffe7bd3a23754a12d96cdf315503
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-30 15:39:18 -0700

    Reading a text file in python

--- File Handling (61 - 70)/Day 61/Read a text file in python.py
+++ File Handling (61 - 70)/Day 61/Read a text file in python.py
@@ -1,3 +1,9 @@
 '''
     This is a simple python program to read the content of text file in python
 '''
+try:
+    with open("C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("This file does not exist!")
+
