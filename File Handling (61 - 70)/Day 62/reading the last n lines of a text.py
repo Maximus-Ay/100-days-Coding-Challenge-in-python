2d97a54a3a9bcc3ef3ab78055cc8ce3deaa53198
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-31 12:30:45 -0700

    Reading the last n lines of a text file

--- File Handling (61 - 70)/Day 62/reading the last n lines of a text.py
+++ File Handling (61 - 70)/Day 62/reading the last n lines of a text.py
@@ -2,3 +2,16 @@
     This is a python program that reads the last n lines of a text file
 '''
 
+def read_last_n_lines(fileName, n):
+    try:
+        with open(fileName, "r") as file:
+            lines = file.readlines()
+            last_n_lines = lines[-n:]
+            for line in last_n_lines:
+                print(line.strip(), end="")
+                print()
+    except FileNotFoundError:
+        print(f"File {fileName} not found!")
+
+fileName = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+read_last_n_lines(fileName, 4)
