ec5556c4f89ee21b9b3965f26c699eda560213c3
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-31 12:34:13 -0700

    Count number of lines in a text file

--- File Handling (61 - 70)/Day 62/python program that counts the number of lines in a text file.py
+++ File Handling (61 - 70)/Day 62/python program that counts the number of lines in a text file.py
@@ -2,3 +2,13 @@
     This is a python program that counts the number of lines in a text file
 '''
 
+def count_number_of_lines(fileName):
+    try:
+        with open(fileName, "r") as file:
+            n = file.readlines()
+            return len(n)
+    except FileNotFoundError:
+        print("The file doesn't exist!")
+
+fileName = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+print(count_number_of_lines(fileName))
