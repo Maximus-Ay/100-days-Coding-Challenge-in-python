80cd959823815c9162d8b3efd1494ab7a400d1b0
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-02 12:17:18 -0700

    Update Program to read a file line by line and store it in a list.py

--- File Handling (61 - 70)/Day 64/Program to read a file line by line and store it in a list.py
+++ File Handling (61 - 70)/Day 64/Program to read a file line by line and store it in a list.py
@@ -2,3 +2,14 @@
     This is a simple python program that reads a file line by line and stores it in a list
 '''
 
+def Read_file_line_by_line(fileName):
+    try:
+        with open(fileName, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line.strip())
+    except FileNotFoundError:
+        print("The file doesn't exist or is not found!")
+
+fileName = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+Read_file_line_by_line(fileName)
