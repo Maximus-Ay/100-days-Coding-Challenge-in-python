2a87851761c6029b7ea2448e99841d5d504f1c81
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-02 12:22:37 -0700

    Find the frequency of words in a text file

--- File Handling (61 - 70)/Day 64/find the frequency of each word in a text file.py
+++ File Handling (61 - 70)/Day 64/find the frequency of each word in a text file.py
@@ -1,3 +1,24 @@
 '''
     This is a python program that finds the frequency of each word in a text file
 '''
+
+def frequency_of_words(fileName):
+    try:
+        with open(fileName, "r") as file:
+            text = file.read()
+            words = text.split()
+            word_count = {}
+            for word in words:
+                word = word.lower()
+                if word in word_count:
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
+            for word, count in word_count.items():
+                print(f"{word}:{count}")
+    except FileNotFoundError:
+        print("The file in question doesn't exitst!")
+
+
+fileName = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+frequency_of_words(fileName)
