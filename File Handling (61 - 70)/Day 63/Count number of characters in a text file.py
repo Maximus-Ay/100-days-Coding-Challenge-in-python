bf8eb16ef816b80c797528cc18e0c040e92b33d7
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-01 20:45:05 -0700

    Update Count number of characters in a text file.py

--- File Handling (61 - 70)/Day 63/Count number of characters in a text file.py
+++ File Handling (61 - 70)/Day 63/Count number of characters in a text file.py
@@ -2,3 +2,14 @@
     This is a simple python program that finds the number of characters in a text file.
 '''
 
+def Count_words(fileName):
+    try:
+        with open(fileName, "r") as file:
+            text = file.read()
+            return len(text)
+    except FileNotFoundError:
+        print("The file you are searching for doesn't exist!")
+
+
+fileName = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+print(Count_words(fileName))
