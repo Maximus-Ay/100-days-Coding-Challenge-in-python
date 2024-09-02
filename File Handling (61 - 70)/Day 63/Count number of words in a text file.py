02f5a011a9d07490009658f57ffa4bdf39ecaca6
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-01 20:37:09 -0700

    Update Count number of words in a text file.py

--- File Handling (61 - 70)/Day 63/Count number of words in a text file.py
+++ File Handling (61 - 70)/Day 63/Count number of words in a text file.py
@@ -2,3 +2,15 @@
     This is a python program to count the number of words in a text file
 '''
 
+def Count_words(fileName):
+    try:
+        with open(fileName, "r") as file:
+            text = file.read()
+            words = text.split()
+            return len(words)
+    except FileNotFoundError:
+        print("The file you are searching for doesn't exist!")
+
+
+fileName = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+print(Count_words(fileName))
