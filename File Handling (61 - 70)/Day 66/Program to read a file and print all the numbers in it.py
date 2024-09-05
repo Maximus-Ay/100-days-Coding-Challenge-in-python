195309febe0392f442151f821ea5cbfcecfbc696
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-04 21:38:34 -0700

    read numbers in a file and print it

--- File Handling (61 - 70)/Day 66/Program to read a file and print all the numbers in it.py
+++ File Handling (61 - 70)/Day 66/Program to read a file and print all the numbers in it.py
@@ -5,8 +5,16 @@
 def Read_File_Return_Values(fileName):
     try:
         with open(fileName, "r") as file:
-            
-
+            text = file.read()
+            numbers = [float(num) for num in text.split() if num.replace('.','', 1).isdigit()]
+            return numbers
+    except FileNotFoundError:
+        print(f"File {fileName} not found!")
+        return []
+    except Exception as e:
+        print(f"Error reading numbers {e}")
+        return []
+                
 
 file1 = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
 print(Read_File_Return_Values(file1))
