0b92ff608adaa0b1ed488468e65300ba6fa5a8dd
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-04 21:29:16 -0700

    Create Program to read a file and print all the numbers in it.py

---
+++ File Handling (61 - 70)/Day 66/Program to read a file and print all the numbers in it.py
@@ -0,0 +1,10 @@
+'''
+    This is a python program to read a file, and print all its numbers or numeric values
+'''
+
+def Read_File_Return_Values(fileName):
+    pass
+
+
+file1 = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+print(Read_File_Return_Values(file1))
