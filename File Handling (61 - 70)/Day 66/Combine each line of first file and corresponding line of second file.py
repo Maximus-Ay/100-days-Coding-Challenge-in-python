f0f77e3d7b32627e6107785e778ea81d65c40644
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-04 21:19:03 -0700

    He

--- File Handling (61 - 70)/Day 66/Combine each line of first file and corresponding line of second file.py
+++ File Handling (61 - 70)/Day 66/Combine each line of first file and corresponding line of second file.py
@@ -2,3 +2,10 @@
     This is a python program that combines each line of the first file and the 
     coresponding line of the second file
 '''
+
+def Combine_files(file1, file2, output_file):
+    pass
+
+file1 = ""
+file2 = ""
+Combine_files(file1, file2, "output.txt")
