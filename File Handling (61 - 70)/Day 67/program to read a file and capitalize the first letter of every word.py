39d014ef6bc8aaf3897427d76fd2d4e90b33ea62
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-05 22:42:57 -0700

    Capitalise the first letter of each word in text file

--- File Handling (61 - 70)/Day 67/program to read a file and capitalize the first letter of every word.py
+++ File Handling (61 - 70)/Day 67/program to read a file and capitalize the first letter of every word.py
@@ -2,3 +2,22 @@
     Python program to read a file and capitalise every first letter of every word
 '''
 
+def capitalise_first_letter(filename):
+    try:
+        with open(filename, "r") as file:
+            text = file.read()
+        words = text.split()
+        capitalized_words = [word.capitalize() for word in words]
+        capitalized_text = ''.join(capitalized_words)
+
+        # Writing to the file
+        with open(filename, "w") as file:
+            file.write(capitalized_text)
+        print(f"First letters capitalized in {filename}.")
+    except FileNotFoundError:
+        print("The file in question was not found among")
+    except Exception as e:
+        print(f"Error capitalising letters {e}")
+
+filename = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+capitalise_first_letter(filename)
