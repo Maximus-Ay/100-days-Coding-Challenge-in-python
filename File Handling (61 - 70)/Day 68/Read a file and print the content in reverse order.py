dbfe02f0b299bf9bdd987eb7eadbf469ee7bfa8a
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-06 19:02:40 -0700

    Read a file and print its content in the reverse order

--- File Handling (61 - 70)/Day 68/Read a file and print the content in reverse order.py
+++ File Handling (61 - 70)/Day 68/Read a file and print the content in reverse order.py
@@ -2,3 +2,16 @@
     This is a python program that reads a file and prints its content in reverse order
 '''
 
+def print_file_in_reverse(filename):
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
+            for line in reversed(lines):
+                print(line.strip()[::-1])
+    except FileNotFoundError:
+        print("The file in question is not found!")
+    except Exception as e:
+        print(f"Erro reading file: {e}")
+    
+filename = "C:\\Users\\Max The Developer\\Documents\\GitHub\\100-days-Coding-Challenge-with-python\\File Handling (61 - 70)\\TestTextFile.txt"
+print_file_in_reverse(filename)
