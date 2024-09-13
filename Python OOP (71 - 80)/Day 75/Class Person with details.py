19f6dbfa43d52252509fa38a655a301d3e87c795
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-13 15:38:20 -0700

    Class Person with details

--- Python OOP (71 - 80)/Day 75/Class Person with details.py
+++ Python OOP (71 - 80)/Day 75/Class Person with details.py
@@ -2,3 +2,13 @@
     This is a class called Person with attributes Name and Age and method to display the details
 '''
 
+class Person:
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+
+    def display_details(self):
+        print(f"My name is {self.name} and I am {self.age} years old!")
+
+me = Person("Maximus", 20)
+me.display_details()
