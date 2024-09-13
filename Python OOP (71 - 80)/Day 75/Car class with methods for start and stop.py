5d329bc67a8991f222cd6cf0a1dc5ecc76f1ea92
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-13 15:34:47 -0700

    Car class with start and stop method

--- Python OOP (71 - 80)/Day 75/Car class with methods for start and stop.py
+++ Python OOP (71 - 80)/Day 75/Car class with methods for start and stop.py
@@ -2,3 +2,18 @@
     Simple car class with methods for the start and stop 
 '''
 
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start(self):
+        return f"The {self.model} {self.year} has been started!"
+    
+    def stop(self):
+        return f"The {self.model} {self.year} has been stopped!"
+    
+myCar = Car("Yo", "BMW", "2021")
+print(myCar.start())
+print(myCar.stop())
