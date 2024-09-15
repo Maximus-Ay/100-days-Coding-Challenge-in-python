a80ab26e18a4066f8db009db504b8b8d4fe40efb
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-15 15:47:54 -0700

    Create Shape.py

---
+++ Python OOP (71 - 80)/Day 77/Shape.py
@@ -0,0 +1,5 @@
+'''
+    This is a python program that creates a class shape and defines methods to calculate the area and perimeter
+    It also includes subclasses like Triangle, Circle and Rectangle
+'''
+
