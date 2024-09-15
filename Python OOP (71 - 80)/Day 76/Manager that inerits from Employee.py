0c089ef32e681f306f7b45dd3caa5aeb30c07b2d
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-14 22:19:10 -0700

    Manager inherits from Employee

--- Python OOP (71 - 80)/Day 76/Manager that inerits from Employee.py
+++ Python OOP (71 - 80)/Day 76/Manager that inerits from Employee.py
@@ -2,3 +2,12 @@
     This is a class called Manager that inherits from Employee and includes an additional attribute called department
 '''
 
+from Employee import Employee
+
+class Manager(Employee):
+    def __init__(self, name, ID, salary, department):
+        super().__init__(name, ID, salary)
+        self.department = department
+
+myManager = Manager("Maximus", 564, 70000, "Engineering")
+print(myManager.department)
