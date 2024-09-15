4a3c5285c4489964b5ec98ff5746c813b9fe9253
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-14 21:59:51 -0700

    Update Employee.py

--- Python OOP (71 - 80)/Day 76/Employee.py
+++ Python OOP (71 - 80)/Day 76/Employee.py
@@ -2,3 +2,15 @@
     Employee class with attributes name, ID, Salary and methods to calculate the annual salary
 '''
 
+class Employee:
+    def __init__(self,name,ID, salary):
+        self.name = name
+        self.ID = ID
+        self.salary = salary
+
+    def annual_salary(self):
+        return f"$ {12 * self.salary}"
+    
+
+myEmployee = Employee("Maximus", 1245612, 70000)
+print(myEmployee.annual_salary())
