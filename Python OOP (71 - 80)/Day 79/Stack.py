d1f1cf3c02a32f8413c9cd615fa4738203b6e98d
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-09-18 19:42:24 -0700

    Update Stack.py

--- Python OOP (71 - 80)/Day 79/Stack.py
+++ Python OOP (71 - 80)/Day 79/Stack.py
@@ -1,3 +1,44 @@
 '''
     Write a class to represent a Stack with methods to push, pop, and check if the stack is empty.
 '''
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        '''Add an item to the top of the stack'''
+        self.stack.append(item)
+
+    def pop(self):
+        '''Remove and return the item from the top of the stack'''
+        if self.is_empty():
+            raise IndexError("Cannot pop from the empty stack")
+        return self.stack.pop()
+    
+    def is_empty(self):
+        '''Check if the stack is empty'''
+        return len(self.stack) == 0
+    
+    def peek(self):
+        '''Return the number of items in the stack'''
+        return len(self.stack)
+    
+    def __str__(self):
+        '''String representation of the stack'''
+        return str(self.stack)
+    
+# Example Usage
+s = Stack()
+
+print("is stack empty? ", s.is_empty()) # True
+s.push(1)
+s.push(2)
+s.push(3)
+
+print("Stack:", s) # [1,2,3]
+print("Popped item:", s.pop())
+print("Stack:", s) # [1,2]
+print("Peek:",s.peek()) # 2
+print("is stack empty?", s.is_empty()) # False
+
