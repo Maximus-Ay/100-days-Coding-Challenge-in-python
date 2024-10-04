86381199f1a8af96e677c3e983bf34e4a90aad12
Maximus-Ay <164879113+Maximus-Ay@users.noreply.github.com>
2024-10-04 13:07:42 -0700

    Create customer relationship management.py

---
+++ Others (Day 81 - 100)/Day 95/customer relationship management.py
@@ -0,0 +1,4 @@
+'''
+    - Write a Python program to create a class that models a basic customer relationship management (CRM) system 
+      with methods to add, remove, and display customer information.
+'''
